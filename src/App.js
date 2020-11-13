29b6aecc03b44110118a6a416cefb72af3fbdfa7
rahul975337 <tilwani03@gmail.com>
2020-11-13 15:01:35 +0530

    DONE

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 import { FaQuoteRight } from "react-icons/fa";
 import data from "./data";
 function App() {
+  // eslint-disable-next-line
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
   // /////////////////////////////
